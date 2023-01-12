29f2f2b429f3ea1400bd52b5c8f476d5712a9ca0
rexxDigital <hello@rexx.icu>
2023-01-12 05:28:18 +0100

    initial commit

---
+++ CMakeFiles/goldrexx.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/goldrexx.dir/main.cpp.o"
+  "CMakeFiles/goldrexx.dir/main.cpp.o.d"
+  "goldrexx"
+  "goldrexx.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/goldrexx.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ GoldRexx/CMakeFiles/GoldRexx.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/libGoldRexx.a"
+  "../bin/libGoldRexx.pdb"
+  "CMakeFiles/GoldRexx.dir/src/Core/main.cpp.o"
+  "CMakeFiles/GoldRexx.dir/src/Core/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GoldRexx.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
